191b3ccb18bb1a28a5695e2c92d883549bb8ac5d
Cloud User <centos@ip-172-31-92-223.ec2.internal>
2020-01-13 04:33:25 +0000

    code changes in test

--- src/test/java/com/ravi/cal/RaviCalculator/CalculatorTest.java
+++ src/test/java/com/ravi/cal/RaviCalculator/CalculatorTest.java
@@ -5,11 +5,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
-    @Test
-    public void twoAndThreeIsFive() throws Exception {
-        final long result = new Calculator().addFucn(2, 3);
-        assertThat(result, is(5L));
-    }
+
 
     @Test
     public void threeMinusTwoIsOne() throws Exception {
@@ -17,11 +13,6 @@ public class CalculatorTest {
         assertThat(result, is(1L));
     }
     
-    @Test
-    public void threeXThreeIsNine() throws Exception {
-        final long result = new Calculator().mulFucn(3, 3);
-        assertThat(result, is(9L));
-    }
-   
+      
 }
 
